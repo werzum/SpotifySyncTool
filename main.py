3f71a3a3084ddb5577f1161e36c1e965d9ece2bc
Carl Orge Retzlaff <e.awerg@web.de>
2021-10-10 15:38:25 +0200

    added function to fetch playlist names with spotipy - next up: display this in a synced list box

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
 import os
 import subprocess
 import PySimpleGUI as sg
+import spotipy
+import re
+from spotipy.oauth2 import SpotifyClientCredentials
+from dotenv import load_dotenv
+
+#load .env file
+load_dotenv()
+client_id = os.getenv("client_id")
+client_secret = os.getenv("client_secret")
+
+#authenticate with spotify to fetch info about playlist
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=client_id,
+                                                           client_secret=client_secret))
+
+#matches the last / of the url
+pattern = ".+(\/.+)$"
+
+def fetch_playlist_name(playlist_url):
+	#extract the id from the url, applying the regex from above and removing the contained
+	#/ with this workaround - dont want to fiddle with the regex yet
+	playlist_id = re.search(pattern, playlist_url).group(1)[1:]
+	result = sp.user_playlist(user=None, playlist_id=playlist_id, fields="name")
+	return result["name"]
 
 
 playlist_urls = [
@@ -15,6 +38,8 @@ playlist_urls = [
 "https://open.spotify.com/playlist/71pk02pQkKzww443kUf21h?si=dc3c1803a3f24827",
 "https://open.spotify.com/playlist/1T8oQMP9iCSGwbbQvg3Rab?si=b8b263e1f2964319"]
 
+playlist_names = [fetch_playlist_name(x) for x in playlist_urls]
+
 def sync(playlists):
     sg.Popup('Sync Started', keep_on_top=True)
     for value in playlist_urls:
@@ -28,7 +53,7 @@ layout = [[sg.Text("Playlists to synchronize")],
           #[sg.Multiline(playlist_urls,key='playlists',size=(100, 20))],
           [sg.Listbox(values=playlist_urls,size=(80,10),key="listbox")],
           [sg.Input(key="input")],
-          [sg.Button("Add"),sg.Button("Remove")],
+          [sg.Button("Add"),sg.Button("Remove"),sg.Button("Remove_All")],
           [sg.Button('Sync'), sg.Button('Quit')]]
 
 
@@ -52,6 +77,10 @@ while True:
 
     if event == "Sync":
         sync(playlist_urls)
+
+    if event == "Remove_All":
+        playlist_urls = []
+        window["listbox"].update(playlist_urls)
 
     # See if user wants to quit or window was closed
     if event == sg.WINDOW_CLOSED or event == 'Quit':

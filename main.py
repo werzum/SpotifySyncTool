fb7d4559a3671c1c9d7cac4ba11eca36acff26a8
Carl Orge Retzlaff <e.awerg@web.de>
2021-10-11 22:56:02 +0200

    Refactored code

--- main.py
+++ main.py
@@ -8,119 +8,146 @@ from dotenv import load_dotenv
 import validators
 import csv
 
-#load .env file
-load_dotenv()
-client_id = os.getenv("client_id")
-client_secret = os.getenv("client_secret")
 
-#authenticate with spotify to fetch info about playlist
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=client_id,
-                                                           client_secret=client_secret))
+def startup():
+	sp = create_spotify_client()
 
-#matches the last / of the url
+	#read the urls from the CSV
+	playlist_urls = playlist_urls()
+
+	#and fetch the respective names from Spotify
+	playlist_names = [fetch_playlist_name(x) for x in playlist_urls]
+
+	return sp, playlist_urls, playlist_names
+
+def create_spotify_client():
+	#load .env file
+	load_dotenv()
+	client_id = os.getenv("client_id")
+	client_secret = os.getenv("client_secret")
+
+	#authenticate with spotify to fetch info about playlist
+	return spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=client_id,
+															   client_secret=client_secret))
+
+#regex pattern that matches the last / of the spotify url
 pattern = ".+(\/.+)$"
 
 def fetch_playlist_name(playlist_url):
 	#extract the id from the url, applying the regex from above and removing the contained
 	#/ with this workaround - dont want to fiddle with the regex yet
 	playlist_id = re.search(pattern, playlist_url).group(1)[1:]
-	result = sp.user_playlist(user=None, playlist_id=playlist_id, fields="name")
-	return result["name"]
 
+	try:
+	   result = sp.user_playlist(user=None, playlist_id=playlist_id, fields="name")
+	except:
+		sg.Popup(f"Could not fetch the url {playlist_url}")
+		return Error
+	else:
+	   return result["name"]
 
-#read the urls from the CSV
-playlist_urls = []
-with open("playlist_urls.csv") as csv_file:
-    reader = csv.reader(csv_file)
-    #stupid unpacking hack that surely has a more elegant solution somewhere
-    temp_list = list(reader)
-    for elm in temp_list[0]:
-        playlist_urls.append(elm)
-
-playlist_names = [fetch_playlist_name(x) for x in playlist_urls]
-#playlist_names=[]
+def read_playlist_urls()
+	#read the urls from the CSV
+	playlist_urls = []
+	with open("playlist_urls.csv") as csv_file:
+		reader = csv.reader(csv_file)
+		#stupid unpacking hack that surely has a more elegant solution somewhere
+		temp_list = list(reader)
+		for elm in temp_list[0]:
+			playlist_urls.append(elm)
+	return playlist_urls
 
 def sync(playlists):
-    sg.Popup('Sync Started', keep_on_top=True)
-    for value in playlist_urls:
-        subprocess.Popen('spotify_dl -l '+value+ " -o", shell=True)
+	sg.Popup('Sync Started', keep_on_top=True)
+	for value in playlist_urls:
+		subprocess.Popen('spotify_dl -l '+value+ " -o", shell=True)
 
 def update_playlists(window,playlist_urls,playlist_names):
-    window["listbox_url"].update(playlist_urls)
-    window["listbox_name"].update(playlist_names)
+	window["listbox_url"].update(playlist_urls)
+	window["listbox_name"].update(playlist_names)
 
 def remove_from_playlist(window,playlist_urls,playlist_names,index):
-    playlist_urls.pop(index)
-    playlist_names.pop(index)
-    update_playlists(window,playlist_urls,playlist_names)
+	playlist_urls.pop(index)
+	playlist_names.pop(index)
+	update_playlists(window,playlist_urls,playlist_names)
 
 def check_valid_url(url):
-    return validators.url(url)
+	return validators.url(url)
 
 def save_urls_to_csv(playlist_urls):
-    with open("playlist_urls.csv","w") as csv_file:
-        write = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
-        write.writerow(playlist_urls)
+	with open("playlist_urls.csv","w") as csv_file:
+		write = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+		write.writerow(playlist_urls)
 
-# Define the window's contents
-layout = [[sg.Frame(layout=[
-            [sg.Listbox(values=playlist_urls,size=(80,10),key="listbox_url")],
-            [sg.Listbox(values=playlist_names,size=(80,10),key="listbox_name")],
-        ],title="Playlists")],
-        [sg.Input(key="input")],
-        [sg.Button("Add"),sg.Button("Remove"),sg.Button("Remove_All")],
-        [sg.Button('Sync'), sg.Button('Quit'), sg.Button("Save")]]
+def create_window_elements():
+	# Define the window's contents
+	layout = [[sg.Frame(layout=[
+				[sg.Listbox(values=playlist_urls,size=(80,10),key="listbox_url")],
+				[sg.Listbox(values=playlist_names,size=(80,10),key="listbox_name")],
+			],title="Playlists")],
+			[sg.Input(key="input",enable_events=True)],
+			[sg.Button("Add",bind_return_key=True),sg.Button("Remove"),sg.Button("Remove_All")],
+			[sg.Button('Sync'), sg.Button('Quit'), sg.Button("Save")]]
 
 
-# Create the window
-window = sg.Window('Spotify Sync', layout)
+	# Create the window
+	window = sg.Window('Spotify Sync', layout)
 
-# Display and interact with the Window using an Event Loop
-while True:
-    event, values = window.read()
+def main():
+	sp, playlist_urls, playlist_names = startup()
 
-    if event == "Add":
-        input = values["input"]
-        if check_valid_url(input):
-            playlist_urls.append(input)
-            playlist_names.append(fetch_playlist_names(input))
-            window["input"].value = ""
-            update_playlists(window,playlist_urls,playlist_names)
-        else:
-            sg.Popup('Malformed URL')
+	window, layout = create_window_elements()
 
-    if event == "Remove":
+	# Display and interact with the Window using an Event Loop
+	while True:
+		event, values = window.read()
 
-        try:
-            index = window.Element('listbox_url').Widget.curselection()[0]
-        except:
-            try:
-                index = window.Element('listbox_name').Widget.curselection()[0]
-            except:
-                sg.Popup("Select a playlist URL or name to be removed")
-            else:
-                remove_from_playlist(window,playlist_urls,playlist_names,index)    
-        else:
-            remove_from_playlist(window,playlist_urls,playlist_names,index)
+		if event == "Add":
+			input = values["input"]
+			if check_valid_url(input):
+				try:
+					playlist_names.append(fetch_playlist_name(input))
+				except:
+					print("error when fetching playlist name")
+				else:
+					playlist_urls.append(input)
+				
+				window["input"].value = ""
+				update_playlists(window,playlist_urls,playlist_names)
+			else:
+				sg.Popup('Malformed URL')
 
-    if event == "Sync":
-        sync(playlist_urls)
+		if event == "Remove":
+			try:
+				index = window.Element('listbox_url').Widget.curselection()[0]
+			except:
+				try:
+					index = window.Element('listbox_name').Widget.curselection()[0]
+				except:
+					sg.Popup("Select a playlist URL or name to be removed")
+				else:
+					remove_from_playlist(window,playlist_urls,playlist_names,index)    
+			else:
+				remove_from_playlist(window,playlist_urls,playlist_names,index)
 
-    if event == "Remove_All":
-        playlist_urls = []
-        playlist_names = []
-        update_playlists(window,playlist_urls,playlist_names)
+		if event == "Sync":
+			sync(playlist_urls)
 
-    if event == "Save":
-        save_urls_to_csv(playlist_urls)
+		if event == "Remove_All":
+			playlist_urls = []
+			playlist_names = []
+			update_playlists(window,playlist_urls,playlist_names)
 
-    # See if user wants to quit or window was closed
-    if event == sg.WINDOW_CLOSED or event == 'Quit':
-        break
+		if event == "Save":
+			save_urls_to_csv(playlist_urls)
 
-# Finish up by removing from the screen
-window.close()
+		# See if user wants to quit or window was closed
+		if event == sg.WINDOW_CLOSED or event == 'Quit':
+			break
+
+	# Finish up by removing from the screen
+	window.close()
 
 
 if __name__ == "__main__":
-    main()
+	main()
